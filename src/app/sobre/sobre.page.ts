ea934ecf954db22d0f3862534bbcc13af38bfbf5
Angel <angelhueso89@hotmail.com>
2022-03-04 12:52:49 +0100

    Mapa

--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-sobre',
@@ -7,6 +8,10 @@ import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
   styleUrls: ['./sobre.page.scss'],
 })
 export class SobrePage implements OnInit {
+
+  map: L.Map;
+  newMarker:any;
+  address:string[];
 
   constructor(private callNumber: CallNumber) { }
 
@@ -17,6 +22,19 @@ export class SobrePage implements OnInit {
     this.callNumber.callNumber("674586553", true)
     .then(res => console.log('Launched dialer!', res))
     .catch(err => console.log('Error launching dialer', err));
+  }
+
+  ionViewDidEnter(){
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latitud = 36.6797047;
+    let longitud = -5.4470656;
+    let zoom = 17;
+    this.map = L.map("mapId").setView([latitud, longitud], zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        .addTo(this.map);
   }
 
 }
